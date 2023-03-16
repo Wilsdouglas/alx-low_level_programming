f27b0c1dceb5ccbb87cfc5cfedb249e954b952d3
Wilsdouglas <douglaswilson813@yahoo.com>
2023-03-16 14:58:39 -0700

    Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts(*\"Programming is like buildng a multilingual puzzle");
+	return (0);
+}
