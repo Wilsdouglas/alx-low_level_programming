836fe4870c92c2cb62b7c928b3d9421a99af85e5
Wilsdouglas <douglaswilson813@yahoo.com>
2023-03-16 16:28:08 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <studio.h>
 
 /**
- * main - Entry point
+ * main - this is the main function
  *
- * Description: Print a code line using puts
- *
- * Return: (0)
-*/
+ * Return: always 0
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilinggual puzzle);
+	return (0)
 }
