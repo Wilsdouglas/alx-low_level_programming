49284aa74949afc792c1fe00c2b5fa79b6625a15
Wilsdouglas <douglaswilson813@yahoo.com>
2023-03-16 18:22:46 -0700

    Quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - my point
+ *
+ * description:: print a quote using write function
+ * size_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error)
+ */
+
+int main(void)
+{
+char quo[] = "and that piece of art is useful\' -Dora Korpar , 2015-10-19\n";
+write(1, quo, 59);
+return (1);
+}
