e867278673647a6e126004ffb3f9728d6e9d665e
Wilsdouglas <douglaswilson813@yahoo.com>
2023-03-16 17:17:30 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Write a programm with printf
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
