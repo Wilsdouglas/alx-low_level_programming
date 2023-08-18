0380096e1e7847c4ccc3c6304be061b93dfef645
Wilsdouglas <douglaswilson813@yahoo.com>
2023-08-17 20:00:07 -0700

    0x17-doubly_linked_list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index of
+ * dlistint_t linked list
  *
  * @head: head of the list
  * @index: index of the new node
